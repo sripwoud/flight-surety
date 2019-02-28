8ed8b4a358f7f65cf2bdeb61281563b18edcc05f
sripwoud <me@sripwoud.xyz>
2019-02-28 17:33:36 +0100

    test: Require multiparty consensus of 50% starting from 4 airlines

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -53,20 +53,70 @@ contract('Flight Surety Tests', async (accounts) => {
     await config.flightSuretyData.authorizeCaller(config.testAddresses[2])
     assert(await config.flightSuretyData.authorizedCallers.call(config.testAddresses[2]))
   })
-/*
-  it('((Data Contract) Can register an airline when less than 4 airlines are registered', async () => {
-    let airline = await config.flightSuretyData.airlines.call(config.testAddresses[3])
-    assert(!airline.registered, 'Airline should not be already registered')
-    await config.flightSuretyData.registerAirline(config.testAddresses[3])
-    airline = await config.flightSuretyData.airlines.call(config.testAddresses[3])
-    assert(airline.registered, 'Airline should be registered')
-  })
-*/
+
   it('(Data Contract) Registers first airline at deployment', async () => {
     assert.equal(await config.flightSuretyData.firstAirline.call(), config.firstAirline)
     assert.equal(await config.flightSuretyData.registeredAirlinesCount.call(), 1)
     const firstAirline = await config.flightSuretyData.airlines.call(config.firstAirline)
     assert(await firstAirline.registered)
+  })
+
+  it('((multiparty) Only first Airline can register an airline when less than 4 airlines are registered', async () => {
+    // Fail if airline not registered
+    try {
+      await config.flightSuretyApp.registerAirline(config.testAddresses[3], { from: accounts[4] })
+    } catch (error) {
+      assert(error.message.includes('Airline must be registered'), 'Error wrong revert message')
+    }
+
+    // register one other airline
+    await config.flightSuretyApp.registerAirline(
+      accounts[2],
+      { from: config.firstAirline })
+    const airline = await config.flightSuretyData.airlines.call(accounts[2])
+    assert(await airline.registered)
+
+    // Fail if not first airline as long as less than 4 airlines registered
+    try {
+      await config.flightSuretyApp.registerAirline(accounts[3], { from: accounts[2] })
+    } catch (error) {
+      assert(error.message.includes('Less than 4 airlines registered'), 'Error wrong revert message')
+    }
+  })
+
+  it('(multiparty) Starting from 4 airlines, half of the registered airlines must agree to register a new one', async () => {
+    // register 2 new airlines
+    await config.flightSuretyApp.registerAirline(
+      accounts[3],
+      { from: config.firstAirline })
+    await config.flightSuretyApp.registerAirline(
+      accounts[4],
+      { from: config.firstAirline })
+    assert.equal(await config.flightSuretyData.registeredAirlinesCount.call(), 4)
+
+    // First airline fails to register 5th one
+    await config.flightSuretyApp.registerAirline(accounts[5], { from: config.firstAirline })
+    let airline = await config.flightSuretyData.airlines.call(accounts[5])
+    assert.equal(await airline.registered, false, 'Error: 5th airline should not have been registered')
+
+    // Cannot vote twice
+    try {
+      await config.flightSuretyApp.registerAirline(accounts[5], { from: config.firstAirline })
+    } catch (error) {
+      assert(error.message.includes('Caller cannot call this function twice'), 'Error: wrong revert message')
+    }
+
+    // Let second other airline vote
+    await config.flightSuretyApp.registerAirline(accounts[5], { from: accounts[2] })
+
+    airline = await config.flightSuretyData.airlines.call(accounts[5])
+    assert(await airline.registered, 'Error: 5th airline was not registered')
+  })
+
+
+/*
+  it('(App Contract) Test var at deployment', async () => {
+    assert.equal(await config.flightSuretyApp.test.call(), config.flightSuretyData.address)
   })
 /*
   it('(airline) cannot register an Airline using registerAirline() if it is not funded', async () => {
