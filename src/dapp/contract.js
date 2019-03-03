5064e51ceb8f4939377c70919f97606b53130788
sripwoud <me@sripwoud.xyz>
2019-03-03 15:06:02 +0100

    feat: Use metamask provider to inject web3
    Inject web3 differently depending on client being a modern/legacy/non 
    dapp browser.
    Additional change in contract.js: use operational public state var 
    instead of a redundant getter isOperational

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -5,7 +5,26 @@ import Web3 from 'web3'
 export default class Contract {
   constructor (network, callback) {
     let config = Config[network]
-    this.web3 = new Web3(new Web3.providers.HttpProvider(config.url))
+    // Inject web3
+    if (window.ethereum) {
+      // use metamask's providers
+      // modern browsers
+      this.web3 = new Web3(window.ethereum)
+      // Request accounts access
+      try {
+        window.ethereum.enable()
+      } catch (error) {
+        console.error('User denied access to accounts')
+      }
+    } else if (window.web3) {
+      // legacy browsers
+      this.web3 = new Web3(web3.currentProvider)
+    } else {
+      // fallback for non dapp browsers
+      this.web3 = new Web3(new Web3.providers.HttpProvider(config.url))
+    }
+
+    // Load contract
     this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress)
     this.initialize(callback)
     this.owner = null
@@ -15,24 +34,28 @@ export default class Contract {
 
   initialize (callback) {
     this.web3.eth.getAccounts((error, accts) => {
-      this.owner = accts[0]
-      let counter = 1
-      while (this.airlines.length < 5) {
+      if (!error) {
+        this.owner = accts[0]
+        let counter = 1
+        while (this.airlines.length < 5) {
           this.airlines.push(accts[counter++])
-      }
+        }
 
-      while(this.passengers.length < 5) {
+        while (this.passengers.length < 5) {
           this.passengers.push(accts[counter++])
-      }
+        }
 
-      callback()
+        callback()
+      } else {
+        console.erro(error)
+      }
     })
   }
 
   isOperational (callback) {
     let self = this
     self.flightSuretyApp.methods
-      .isOperational()
+      .operational()
       .call({ from: self.owner }, callback)
   }
 
