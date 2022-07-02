c55c45899f9a233a36e7cd29ec11ef2f2d512243
r1oga <38692952+r1oga@users.noreply.github.com>
2022-07-02 20:53:15 +0200

    get signers list

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -1,2 +1,2 @@
 export * from './contracts'
-export { default as wallet } from './wallet'
+export { default as wallet } from './signers'
--- src/eth/wallet.ts
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers'
-
-import provider from './provider'
-
-const wallet = ethers.Wallet.fromMnemonic(process.env.mnemonic!)
-wallet.connect(provider)
-
-export default wallet
