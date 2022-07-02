ac20b4e3bdd3909abc082c49d79cddeb109439d1
r1oga <38692952+r1oga@users.noreply.github.com>
2022-07-02 21:12:54 +0200

    refactor deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,6 @@
 const { exec } = require('child_process')
 const { ethers } = require('ethers')
 
-const dataArtifact = require('../contracts/out/FlightSuretyData.sol/FlightSuretyData.json')
-const appArtifact = require('../contracts/out/FlightSuretyApp.sol/FlightSuretyApp.json')
 const config = require('../config.json')
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
@@ -27,51 +25,37 @@ const updateInConfigFile = (fieldName, value) => {
   exec(`${DASEL_CMD} '.${fieldName}' ${value}`, cb)
 }
 
-const deploy = async (contractName) => {
-  const address = config[`${contractName.toLowerCase()}Address`]
+const deploy = async (contractName, params) => {
+  const fieldName = `${contractName.toLowerCase()}Address`
+  const address = config[fieldName]
   if (address) {
-    const bytecode = await provider.send('eth_getCode', [address])
-    console.log({ bytecode })
+    const bytecode = await provider.send('eth_getCode', [address, 'latest'])
     const exists = bytecode === '0x'
-    if (exists) {
+    if (!exists) {
       console.log(`${contractName} Contract already deployed at ${address}`)
       return
     }
-
-    const artifact = require(`../contracts/out/FlightSuretyApp.sol/FlightSurety${contractName}.json`)
   }
 
-  const Data = new ethers.ContractFactory(
-    dataArtifact.abi,
-    dataArtifact.bytecode,
+  const artifact = require(`../contracts/out/FlightSurety${contractName}.sol/FlightSurety${contractName}.json`)
+  const Contract = new ethers.ContractFactory(
+    artifact.abi,
+    artifact.bytecode,
     signer
   )
-  const data = await Data.deploy(signer.address)
-  await data.deployed()
-  console.log(`Data Contract deployed at ${data.address}`)
-  updateInConfigFile('dataAddress', data.address)
+
+  const contract = await Contract.deploy(...params)
+
+  await contract.deployed()
+  console.log(`${contractName} Contract deployed at ${contract.address}`)
+  updateInConfigFile(fieldName, contract.address)
+
+  return contract
 }
 
 async function main() {
-  const Data = new ethers.ContractFactory(
-    dataArtifact.abi,
-    dataArtifact.bytecode,
-    signer
-  )
-  const data = await Data.deploy(signer.address)
-  await data.deployed()
-  console.log(`Data Contract deployed at ${data.address}`)
-  updateInConfigFile('dataAddress', data.address)
-
-  const App = new ethers.ContractFactory(
-    appArtifact.abi,
-    appArtifact.bytecode,
-    signer
-  )
-  const app = await App.deploy(data.address)
-  await app.deployed()
-  console.log(`App Contract deployed at ${app.address}`)
-  updateInConfigFile('appAddress', app.address)
+  const data = await deploy('Data', [signer.address])
+  await deploy('App', [data.address])
 }
 
 // We recommend this pattern to be able to use async/await everywhere
