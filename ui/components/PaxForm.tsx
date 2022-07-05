18ed239ebb596222ebd3c52edff3ce267a35820c
r1oga <38692952+r1oga@users.noreply.github.com>
2022-07-05 13:34:01 +0200

    fix typo

--- ui/components/PaxForm.tsx
+++ ui/components/PaxForm.tsx
@@ -5,7 +5,7 @@ import FLightsList from './Flights/FlightList'
 const PaxForm = () => {
   return (
     <>
-      <FLightsList title="Book FLight" />
+      <FLightsList title="Book Flight" />
       <Divider />
       <FLightsList booked title="Booked Fights" />
     </>
