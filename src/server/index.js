cdbbd6e9f369bacc26ac899f32063c89f2b5a60e
sripwoud <me@sripwoud.xyz>
2022-07-06 09:42:15 +0200

    style: Follow JS standard

--- src/server/index.js
+++ src/server/index.js
@@ -7,9 +7,9 @@ let currentApp = app
 server.listen(3000)
 
 if (module.hot) {
- module.hot.accept('./server', () => {
-  server.removeListener('request', currentApp)
-  server.on('request', app)
-  currentApp = app
- })
+  module.hot.accept('./server', () => {
+    server.removeListener('request', currentApp)
+    server.on('request', app)
+    currentApp = app
+  })
 }
