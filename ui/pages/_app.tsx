ef7793acf952d1460d00482d4c3504ac32288aab
sripwoud <me@sripwoud.xyz>
2022-07-06 23:00:31 +0200

    feat(ui): use ropsten chain id and provider

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -8,10 +8,8 @@ import { DAppProvider, Config } from '@usedapp/core'
 import Layout from '../components/Layout'
 
 const config: Config = {
-  readOnlyChainId: 31337,
-  readOnlyUrls: {
-    31337: 'http://127.0.0.1:8545'
-  }
+  readOnlyChainId: 3,
+  readOnlyUrls: { 3: process.env.RPC_URL! }
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
