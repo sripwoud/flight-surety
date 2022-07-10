3ae0994be361abbfdd76b77533f625429c239434
r1oga <38692952+r1oga@users.noreply.github.com>
2022-07-10 19:08:44 +0200

    update server addresses.ts

--- server/src/eth/contracts/addresses.ts
+++ server/src/eth/contracts/addresses.ts
@@ -1,9 +1,9 @@
 const addresses = {
   data:
-    '0x13c55Fe5a4fE91d7865D4be53C260757fC989974' || process.env.DATA_ADDRESS!,
-  app: '0x82C107cb2a851AA920A389896A4467A5E07acc60' || process.env.APP_ADDRESS!,
+    process.env.DATA_ADDRESS! || '0x13c55Fe5a4fE91d7865D4be53C260757fC989974',
+  app: process.env.APP_ADDRESS! || '0x82C107cb2a851AA920A389896A4467A5E07acc60',
   oracles:
-    '0x30877f4Dcf2F99F846439Ce797CE4C3154F79c6A' || process.env.ORACLES_ADDRESS!
+    process.env.ORACLES_ADDRESS! || '0x30877f4Dcf2F99F846439Ce797CE4C3154F79c6A'
 }
 
 export default addresses
