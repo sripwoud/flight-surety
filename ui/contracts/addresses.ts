55a842efd23529cc396a42aa10af75999e0e31da
r1oga <38692952+r1oga@users.noreply.github.com>
2022-07-11 12:17:38 +0200

    revert addresses change

--- ui/contracts/addresses.ts
+++ ui/contracts/addresses.ts
@@ -1,9 +1,7 @@
 const addresses = {
-  data:
-    process.env.DATA_ADDRESS! || '0x13c55Fe5a4fE91d7865D4be53C260757fC989974',
-  app: process.env.APP_ADDRESS! || '0x82C107cb2a851AA920A389896A4467A5E07acc60',
-  oracles:
-    process.env.ORACLES_ADDRESS! || '0x30877f4Dcf2F99F846439Ce797CE4C3154F79c6A'
+  data: '0x13c55Fe5a4fE91d7865D4be53C260757fC989974',
+  app: '0x82C107cb2a851AA920A389896A4467A5E07acc60',
+  oracles: '0x30877f4Dcf2F99F846439Ce797CE4C3154F79c6A'
 }
 
 export default addresses
